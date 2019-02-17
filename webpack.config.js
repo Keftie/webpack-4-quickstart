cb3d6a48f8bcc185c21a9ea53908858e66b423d6
Kevin vdBosch <kevinvdbosch_1@hotmail.com>
2019-02-17 11:43:54 +0100

    install webpack css loader plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     module: {
@@ -11,7 +12,7 @@ module.exports = {
                 }
             },
             {
-                test: /.html$/,
+                test: /\.html$/,
                 use: [
                     {
                         loader: "html-loader",
@@ -20,6 +21,13 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
             }
         ]
     },
@@ -27,6 +35,10 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/index.html",
             filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
         })
     ]
 };
